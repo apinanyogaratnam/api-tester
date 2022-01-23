7cf91cd50fa3356de343e208807cc6e1a602b4da
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2022-01-22 19:39:04 -0500

    refactor: converted from callback to async/await syntax

--- api_tester.js
+++ api_tester.js
@@ -2,9 +2,10 @@ import axios from "axios";
 
 const url = "https://raw.githubusercontent.com/apinanyogaratnam/Computer-Science-Languages-API/main/data.json";
 
-const fetchData = () => {
-    return axios.get(url)
-          .then((response) => console.log(response.data));}
+const fetchData = async () => {
+    const response = await axios.get(url);
 
+    return response.data;
+};
 
-fetchData()
+console.log(fetchData());
